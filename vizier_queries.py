646d65a8630afb89a3ac66ecf186765f55601ff4
Alyssa Bulatek <abulatek@ufl.edu>
2020-07-15 15:53:52 -0500

    Update vizier_queries.py
    Added queries for 2017 survey tables (which are divided based on the tables in the paper, unlike with the 2014 catalog).

--- vizier_queries.py
+++ vizier_queries.py
@@ -1,4 +1,20 @@
 from astroquery.vizier import Vizier
-# Access all columns
-alcala_all_tbl = Vizier(columns=['**']).query_constraints(catalog='J/A+A/561/A2/results')[0]
-alcala_all_tbl.write('alcala_all.csv')
+
+# Access all columns from the Alcala+2014 survey
+alcala2014_all_tbl = Vizier(columns=['**']).query_constraints(catalog='J/A+A/561/A2/results')[0]
+alcala2014_all_tbl.write('alcala2014_all.csv')
+
+# Access all columns from the Alcala+2017 survey (across different tables)
+alcala2017_ra_dec_tbl = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A20/table1')[0]
+alcala2017_tbl1 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A20/tablee1')[0]
+alcala2017_tbl2 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A20/tablee2')[0]
+alcala2017_tbl3 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A20/tablee3')[0]
+alcala2017_tbl4 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A20/tablee4')[0]
+alcala2017_tbl5 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A20/tablee5')[0]
+
+alcala2017_ra_dec_tbl.write('alcala2017_ra_dec.csv')
+alcala2017_tbl1.write('alcala2017_1.csv')
+alcala2017_tbl2.write('alcala2017_2.csv')
+alcala2017_tbl3.write('alcala2017_3.csv')
+alcala2017_tbl4.write('alcala2017_4.csv')
+alcala2017_tbl5.write('alcala2017_5.csv')
