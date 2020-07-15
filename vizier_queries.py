96b2602fe3ee07e2ccc0848deff2c21df1dc9552
Alyssa Bulatek <abulatek@ufl.edu>
2020-07-14 19:05:12 -0500

    Update vizier_queries.py
    Added missing import statement

--- vizier_queries.py
+++ vizier_queries.py
@@ -1,3 +1,4 @@
+from astroquery.vizier import Vizier
 # Access all columns
 alcala_all_tbl = Vizier(columns=['**']).query_constraints(catalog='J/A+A/561/A2/results')[0]
 alcala_all_tbl.write('alcala_all.csv')
