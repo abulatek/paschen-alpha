72bd76d4d520d3a99d1bb3b19fa49846c77e157d
Alyssa Bulatek <abulatek@ufl.edu>
2020-07-17 16:38:16 -0500

    Added a way to join tables

--- vizier_queries.py
+++ vizier_queries.py
@@ -1,4 +1,5 @@
 from astroquery.vizier import Vizier
+from astropy.table import join
 
 # Access all columns from the Alcala+2014 survey
 alcala2014_all_tbl = Vizier(columns=['**']).query_constraints(catalog='J/A+A/561/A2/results')[0]
@@ -12,9 +13,13 @@ alcala2017_tbl3 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A2
 alcala2017_tbl4 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A20/tablee4')[0]
 alcala2017_tbl5 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A20/tablee5')[0]
 
-alcala2017_ra_dec_tbl.write('alcala2017_ra_dec.csv')
+alcala2017_ra_dec_tbl.write('alcala2017_ra_dec.csv') # ended up removing the final 4 rows -- should look up why there isn't spectral line data for those sources
 alcala2017_tbl1.write('alcala2017_1.csv')
 alcala2017_tbl2.write('alcala2017_2.csv')
 alcala2017_tbl3.write('alcala2017_3.csv')
 alcala2017_tbl4.write('alcala2017_4.csv')
 alcala2017_tbl5.write('alcala2017_5.csv')
+
+## Make a table that includes both Halpha data and RA/dec for sources in Alcala+2017
+alcala2017_halpha_ra_dec_tbl = join(alcala2017_ra_dec_tbl, alcala2017_tbl1)
+alcala2017_halpha_ra_dec_tbl.write('alcala2017_halpha_ra_dec.csv')
