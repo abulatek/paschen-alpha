Fixed application of units (re: issue Adam raised)
Also, I took out some commented code in the plotting section for simplicity.

ccd_combined.py
@@ -21,57 +21,51 @@ df_yso_dropped = df_yso.dropna(subset=['FIR1','FIR2','FIR3','FIR4','FHa']) # Did
 
 # Get individual values for background stars
 ## Did not do any grouping here, so there might be duplicated sources (unlikely, though)
-background_Ha_mag = df_background_dropped['Hamag'].values * u.mag
-background_FIR1_Jy = df_background_dropped['FIR1'].values * (10**(-3)) * u.Jy
-background_FIR2_Jy = df_background_dropped['FIR2'].values * (10**(-3)) * u.Jy
-background_FIR3_Jy = df_background_dropped['FIR3'].values * (10**(-3)) * u.Jy
-background_FIR4_Jy = df_background_dropped['FIR4'].values * (10**(-3)) * u.Jy
+background_Ha_mag = u.Quantity(df_background_dropped['Hamag'].values, u.mag)
+background_FIR1_Jy = u.Quantity(df_background_dropped['FIR1'].values, (10**(-3))*u.Jy)
+background_FIR2_Jy = u.Quantity(df_background_dropped['FIR2'].values, (10**(-3))*u.Jy)
+background_FIR3_Jy = u.Quantity(df_background_dropped['FIR3'].values, (10**(-3))*u.Jy)
+background_FIR4_Jy = u.Quantity(df_background_dropped['FIR4'].values, (10**(-3))*u.Jy)
 # For Alcala catalog, remove duplicate rows in xMatch table
 df_yso_grouped = df_yso_dropped.groupby('Object')
 # Then, extract photometry information
-yso_FIR1_Jy = df_yso_grouped['FIR1'].agg(np.mean) * (10**(-3)) * u.Jy
-yso_FIR2_Jy = df_yso_grouped['FIR2'].agg(np.mean) * (10**(-3)) * u.Jy
-yso_FIR3_Jy = df_yso_grouped['FIR3'].agg(np.mean) * (10**(-3)) * u.Jy
-yso_FIR4_Jy = df_yso_grouped['FIR4'].agg(np.mean) * (10**(-3)) * u.Jy
-yso_FHa_flux = df_yso_grouped['FHa'].agg(np.mean) * (10**(-3)) * u.W / (u.meter)**2
+yso_FIR1_Jy = u.Quantity(df_yso_grouped['FIR1'].agg(np.mean), (10**(-3))*u.Jy)
+yso_FIR2_Jy = u.Quantity(df_yso_grouped['FIR2'].agg(np.mean), (10**(-3))*u.Jy)
+yso_FIR3_Jy = u.Quantity(df_yso_grouped['FIR3'].agg(np.mean), (10**(-3))*u.Jy)
+yso_FIR4_Jy = u.Quantity(df_yso_grouped['FIR4'].agg(np.mean), (10**(-3))*u.Jy)
+yso_FHa_flux = u.Quantity(df_yso_grouped['FHa'].agg(np.mean), (10**(-3))*u.W/u.meter**2)
 
 # Convert H-alpha fluxes to average flux densities in mJy
 ## Divide H-alpha flux by frequency of H-alpha in Hz, multiply by factor of 10**(-26)
-frequency_Ha = (6568.0 * u.AA).to(u.Hz, equivalencies=u.spectral()) # Used INT WFC H-alpha filter center wavelength (from http://svo2.cab.inta-csic.es/theory/fps/index.php?id=INT/IPHAS.Ha&&mode=browse&gname=INT&gname2=IPHAS#filter)
-yso_FHa_Jy = yso_FHa_flux*(10**(-26))/frequency_Ha 
+frequency_Ha = (6568.0 * u.AA).to(u.Hz, equivalencies=u.spectral()) # Used INT WFC H-alpha filter center wavelength from http://svo2.cab.inta-csic.es/theory/fps/index.php?id=INT/IPHAS.Ha&&mode=browse&gname=INT&gname2=IPHAS#filter
+yso_FHa_Jy = yso_FHa_flux*(10**(-26))/frequency_Ha
 
 # Convert flux densities to magnitudes for background stars
-## First, define Spitzer IRAC zero points (from http://svo2.cab.inta-csic.es/theory/fps/index.php?mode=browse&gname=Spitzer)
-FIR1_ZP_Jy = 277.2 * u.Jy
-FIR2_ZP_Jy = 179.0 * u.Jy
-FIR3_ZP_Jy = 113.8 * u.Jy
-FIR4_ZP_Jy = 62.0 * u.Jy
-Ha_ZP_Jy = 2609.54 * u.Jy # INT WFC H-alpha filter (from http://svo2.cab.inta-csic.es/theory/fps/index.php?id=INT/IPHAS.Ha&&mode=browse&gname=INT&gname2=IPHAS#filter)
-background_FIR1_mag = -2.5*np.log10(background_FIR1_Jy/FIR1_ZP_Jy) * u.mag
-background_FIR2_mag = -2.5*np.log10(background_FIR2_Jy/FIR2_ZP_Jy) * u.mag
-background_FIR3_mag = -2.5*np.log10(background_FIR3_Jy/FIR3_ZP_Jy) * u.mag
-background_FIR4_mag = -2.5*np.log10(background_FIR4_Jy/FIR4_ZP_Jy) * u.mag
+## First, define Spitzer IRAC zero points from http://svo2.cab.inta-csic.es/theory/fps/index.php?mode=browse&gname=Spitzer
+FIR1_ZP_Jy = 277.2*u.Jy
+FIR2_ZP_Jy = 179.0*u.Jy
+FIR3_ZP_Jy = 113.8*u.Jy
+FIR4_ZP_Jy = 62.0*u.Jy
+Ha_ZP_Jy = 2609.54*u.Jy # INT WFC H-alpha filter from http://svo2.cab.inta-csic.es/theory/fps/index.php?id=INT/IPHAS.Ha&&mode=browse&gname=INT&gname2=IPHAS#filter
+background_FIR1_mag = -2.5*np.log10(background_FIR1_Jy/FIR1_ZP_Jy)*u.mag
+background_FIR2_mag = -2.5*np.log10(background_FIR2_Jy/FIR2_ZP_Jy)*u.mag
+background_FIR3_mag = -2.5*np.log10(background_FIR3_Jy/FIR3_ZP_Jy)*u.mag
+background_FIR4_mag = -2.5*np.log10(background_FIR4_Jy/FIR4_ZP_Jy)*u.mag
 # Do the same for Alcala catalog
-yso_FIR1_mag = -2.5*np.log10(yso_FIR1_Jy/FIR1_ZP_Jy) * u.mag
-yso_FIR2_mag = -2.5*np.log10(yso_FIR2_Jy/FIR2_ZP_Jy) * u.mag
-yso_FIR3_mag = -2.5*np.log10(yso_FIR3_Jy/FIR3_ZP_Jy) * u.mag
-yso_FIR4_mag = -2.5*np.log10(yso_FIR4_Jy/FIR4_ZP_Jy) * u.mag
-yso_FHa_mag = -2.5*np.log10(yso_FHa_Jy/Ha_ZP_Jy) * u.mag
+yso_FIR1_mag = -2.5*np.log10(yso_FIR1_Jy/FIR1_ZP_Jy)*u.mag
+yso_FIR2_mag = -2.5*np.log10(yso_FIR2_Jy/FIR2_ZP_Jy)*u.mag
+yso_FIR3_mag = -2.5*np.log10(yso_FIR3_Jy/FIR3_ZP_Jy)*u.mag
+yso_FIR4_mag = -2.5*np.log10(yso_FIR4_Jy/FIR4_ZP_Jy)*u.mag
+yso_FHa_mag = -2.5*np.log10(yso_FHa_Jy/Ha_ZP_Jy)*u.mag
 
 # Plot color-color diagrams
 plt.figure(dpi = 100)
 plt.grid()
-#plt.rc('font', family='serif')
 plt.scatter(background_Ha_mag-background_FIR1_mag,background_FIR1_mag-background_FIR2_mag,label="background star",color='xkcd:black')
 plt.scatter(yso_FHa_mag-yso_FIR1_mag,yso_FIR1_mag-yso_FIR2_mag,label="YSO",color='xkcd:azure')
 plt.xlabel("H-alpha - [3.6]", size = 14)
-#plt.xlabel(r"$H\alpha - [3.6]$", size = 14)
 plt.ylabel("[3.6] - [4.5]", size = 14)
-#plt.ylabel(r"$[3.6] - [4.5]$", size = 14)
-#plt.xlim(0.7,4.2)
-#plt.ylim(0,1)
 plt.legend(loc="upper right")
 plt.title("Color-color diagram for YSOs and background stars", size = 14)
-#plt.savefig('test_ccd.png', transparent = False)
-#, facecolor='none', edgecolor='none')
+#plt.savefig('test_ccd.png', transparent = False, facecolor='none', edgecolor='none')
 plt.show()