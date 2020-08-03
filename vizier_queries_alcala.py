a20cd761a5bbc142c0b410647f5f5739197ac345
Alyssa Bulatek <abulatek@ufl.edu>
2020-08-03 16:16:07 -0500

    Cleaned up removal of final four rows of RA/dec table

--- vizier_queries_alcala.py
+++ vizier_queries_alcala.py
@@ -13,15 +13,19 @@ alcala2017_tbl1 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A2
 #alcala2017_tbl4 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A20/tablee4')[0]
 #alcala2017_tbl5 = Vizier(columns=['**']).query_constraints(catalog='J/A+A/600/A20/tablee5')[0]
 
-alcala2017_ra_dec_tbl.write('alcala2017_ra_dec.csv') # ended up removing the final 4 rows -- should look up why there isn't spectral line data for those sources
-alcala2017_tbl1.write('alcala2017_1.csv')
+# Remove final four rows of RA and dec table (rejected as Lupus members)
+alcala2017_ra_dec_tbl.remove_rows([-1, -2, -3, -4])
+
+# Export tables individually as .csv files
+#alcala2017_ra_dec_tbl.write('alcala2017_ra_dec.csv')
+#alcala2017_tbl1.write('alcala2017_1.csv')
 #alcala2017_tbl2.write('alcala2017_2.csv')
 #alcala2017_tbl3.write('alcala2017_3.csv')
 #alcala2017_tbl4.write('alcala2017_4.csv')
 #alcala2017_tbl5.write('alcala2017_5.csv')
 
 # Make a table that includes both Halpha data and RA/dec for sources in Alcala+2017
-alcala2017_halpha_ra_dec_tbl = join(alcala2017_ra_dec_tbl, alcala2017_tbl1)
+alcala2017_halpha_ra_dec_tbl = join(alcala2017_ra_dec_tbl, alcala2017_tbl1) # Matches based on common columns
 alcala2017_halpha_ra_dec_tbl.write('alcala2017_halpha_ra_dec.csv')
 
 # Access accretion information from Alcala+2017 survey
