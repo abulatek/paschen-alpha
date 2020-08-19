e21119172914dca573be3ace70ac9d7ba17d7ab4
Alyssa Bulatek <abulatek@ufl.edu>
2020-08-19 12:59:33 -0500

    Changed plot axes, included some diagnostics, dropped less rows

--- ccd_optical.py
+++ ccd_optical.py
@@ -9,9 +9,8 @@ from astropy import units as u
 filepath_background = 'background_xmatch_test.csv'
 df_background = pd.read_csv(filepath_background)
 
-# Drop rows that have NaNs in any columns corresponding to fluxes
-#df_background_dropped = df_background.dropna(subset=['umag', 'gmag', 'rmag', 'imag', 'Hamag'])
-df_background_dropped = df_background
+# Ignore objects that don't have H-alpha data
+df_background_dropped = df_background.dropna(subset=['Hamag','rmag','imag'])
 
 # Get magnitudes
 umag = u.Quantity(df_background_dropped['umag'].values, u.mag)
@@ -24,31 +23,32 @@ Hamag = u.Quantity(df_background_dropped['Hamag'].values, u.mag)
 # Get magnitudes for specific types of YSOs 
 YSO_type_str_list = ['YSOc', 'YSOc_PAH-em', 'YSOc_red', 'YSOc_star+dust(IR1)', 'YSOc_star+dust(IR2)', 'YSOc_star+dust(IR3)', 'YSOc_star+dust(IR4)', 'YSOc_star+dust(MP1)']
 df_YSO = df_background_dropped.loc[df_background_dropped['OType'].isin(YSO_type_str_list)]
+#print(len(df_YSO))
+#print(df_YSO[['sourceID', 'umag', 'gmag', 'r2mag', 'rmag', 'imag']])
 
+# Check out what's up with possibly duplicate YSOs
+df_YSO_sort = df_YSO.sort_values(by=['rmag'])
+print(df_YSO_sort[['sourceID', 'umag', 'gmag', 'r2mag', 'rmag', 'imag']])
 # Plot color-color diagrams
 plt_type = 3
 
 plt.figure(dpi = 150)
 plt.grid()
 if plt_type == 1:
-        plt.scatter(Hamag - rmag, umag - gmag, marker='.', color='black', label='non-YSO', alpha=0.2)
-        x_color = df_YSO['Hamag'].values - df_YSO['rmag'].values
-        y_color = df_YSO['umag'].values - df_YSO['gmag'].values
+        plt.scatter(umag - gmag, rmag - Hamag, marker='.', color='black', label='non-YSO', alpha=0.2)
+        x_color = df_YSO['umag'].values - df_YSO['gmag'].values
+        y_color = df_YSO['rmag'].values - df_YSO['Hamag'].values
         plt.scatter(x_color, y_color, marker='.', color='dodgerblue', label='YSO candidate')
-        plt.ylabel("u - g", size = 14)
+        plt.xlabel("u - g", size = 14)
 if plt_type == 2:
-        plt.scatter(Hamag - rmag, gmag - r2mag, marker='.', color='black', label='non-YSO', alpha=0.2)
-        x_color = df_YSO['Hamag'].values - df_YSO['rmag'].values
-        y_color = df_YSO['gmag'].values - df_YSO['r2mag'].values
+        plt.scatter(gmag - r2mag, rmag - Hamag, marker='.', color='black', label='non-YSO', alpha=0.2)
+        x_color = df_YSO['gmag'].values - df_YSO['r2mag'].values
+        y_color = df_YSO['rmag'].values - df_YSO['Hamag'].values
         plt.scatter(x_color, y_color, marker='.', color='dodgerblue', label='YSO candidate')
-        plt.ylabel("g - r2", size = 14)
+        plt.xlabel("g - r2", size = 14)
 if plt_type == 3:
-        plt.scatter(Hamag - rmag, rmag - imag, marker='.', color='black', label='non-YSO', alpha=0.2)
-        x_color = df_YSO['Hamag'].values - df_YSO['rmag'].values
-        y_color = df_YSO['rmag'].values - df_YSO['imag'].values
-        plt.scatter(x_color, y_color, marker='.', color='dodgerblue', label='YSO candidate')
-        plt.ylabel("r - i", size = 14)
-plt.xlabel("H-alpha - r", size = 14)
-plt.legend(loc="upper right", fontsize = 6)
-plt.title("VPHAS/c2d crossmatch color-color diagram", size = 14)
-plt.show()
+        plt.scatter(rmag - imag, rmag - Hamag, marker='.', color='black', label='non-YSO', alpha=0.2)
+        x_color = df_YSO['rmag'].values - df_YSO['imag'].values
+        y_color = df_YSO['rmag'].values - df_YSO['Hamag'].values
+        #print(len(x_color),len(y_color))
+        plt.scatter(x_color, y_color, marker='.', color='dodgerblue', label='YSO candidate', alpha=0.2)
